672f4adaf5637368cd679be708829893c398714b
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-14 14:44:18 +0000

    fajita: vendorsetup: fixup! * It's fix fod releated errors
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,7 @@
 rm -rf frameworks/base
 git clone https://github.com/chandu078/android_frameworks_base -b 11-fod frameworks/base
+cd packages/apps/Settings
+git remote add fod https://github.com/chandu078/android_packages_apps_Settings-1
+git fetch fod
+git cherry-pick 53edb22031d3d5544e3105691a12ae6982e3e5f1
+cd ../../..
