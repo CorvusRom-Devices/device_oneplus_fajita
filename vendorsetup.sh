669670d7ad12fd1c365e8f0abef2aa4cf8507818
chandu078 <chandudyavanapelli03@gmail.com>
2021-02-03 19:03:08 +0000

    fajita: vendorsetup: Track packages/apps/Settings from 11-fod branch
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,11 @@
+echo "deleting frameworks/base from 11 branch"
 rm -rf frameworks/base
+
+echo "cloning frameworks/base from 11-fod branch"
 git clone https://github.com/Corvus-R/android_frameworks_base -b 11-fod frameworks/base
+
+echo "deleting packages/apps/Settings from 11 branch"
+rm -rf packages/apps/Settings
+
+echo "cloning packages/apps/Settings from 11-fod branch"
+git clone https://github.com/Corvus-R/android_packages_apps_Settings -b 11-fod packages/apps/Settings
